7c4a87d47d8beb81b2568bbb4380b26c6a33e79f
zpyyy01 <pyzhang086@gmail.com>
2024-10-10 10:51:56 +0800

    测试

--- src/main/java/ASTanalyze.java
+++ src/main/java/ASTanalyze.java
@@ -4,6 +4,7 @@ import com.google.gson.JsonObject;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.application.WriteAction;
 import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
@@ -12,6 +13,8 @@ import org.jetbrains.annotations.NotNull;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ASTanalyze extends AnAction {
     @Override
@@ -36,10 +39,41 @@ public class ASTanalyze extends AnAction {
     }
 
     private void assignAnalyze(PsiClass cls, PsiFile psiFile, AssignExpList assignExpList) {
+        Map<String, String> variableAssignments = new HashMap<>();
+        cls.accept(new JavaRecursiveElementVisitor() {
+            @Override
+            public void visitAssignmentExpression(@NotNull PsiAssignmentExpression expression) {
+                super.visitAssignmentExpression(expression);
+                PsiExpression left = expression.getLExpression();
+                PsiExpression right = expression.getRExpression();
 
+                if (left instanceof PsiReferenceExpression && right != null) {
+                    String variableName = ((PsiReferenceExpression) left).getReferenceName();
+                    variableAssignments.put(variableName, right.getText());
+                }
+            }
+        });
+        cls.accept(new JavaRecursiveElementVisitor() {
+            @Override
+            public void visitReferenceExpression(@NotNull PsiReferenceExpression expression) {
+                super.visitReferenceExpression(expression);
+                String newText = replaceVariables(expression.getText(), variableAssignments);
+                if (!newText.equals(expression.getText())) {
+                    WriteAction.run(() -> {
+                        PsiElement newElement = PsiElementFactory.getInstance(psiFile.getProject())
+                                .createExpressionFromText(newText, null);
+                        expression.replace(newElement);
+                    });
+                }
+            }
+        });
 
-        assignExpList.substituteAssignments();
     }
-
+    private String replaceVariables(String expressionText, Map<String, String> variableAssignments) {
+        for (Map.Entry<String, String> entry : variableAssignments.entrySet()) {
+            expressionText = expressionText.replace(entry.getKey(), entry.getValue());
+        }
+        return expressionText;
+    }
 }
 
