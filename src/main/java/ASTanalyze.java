f118e69bc270104a584af88c365fb5df4bf3ea95
zpyyy01 <pyzhang086@gmail.com>
2024-10-10 10:56:48 +0800

    测试

--- src/main/java/ASTanalyze.java
+++ src/main/java/ASTanalyze.java
@@ -64,7 +64,7 @@ public class ASTanalyze extends AnAction {
                                 .createExpressionFromText(newText, null);
                         expression.replace(newElement);
                     });
-                }
+                }5
             }
         });
 
