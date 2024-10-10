1988249aa4b21e7bf244fa49bddd0ecdc7eb1ae4
zpyyy01 <pyzhang086@gmail.com>
2024-10-10 11:01:48 +0800

    测试

--- src/main/resources/Test/testFile.java
+++ src/main/resources/Test/testFile.java
@@ -2,7 +2,8 @@ public class test {
     public static void main(String[] args) {
         int a, b = 10, x = 1, y = 2;
         a = x * y;
-        b = a + y - x * a;
+        b = x - y;
+        c = ( a + b + x) * y;
         System.out.println("a: " + a + ", b: " + b);
     }
 }
